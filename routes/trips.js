78e48bd494bd21bad2460cea3ffebe0f15f7314d
Chacha M <mahfoud.chacha@gmail.com>
2023-04-11 14:52:07 +0200

    maj date

--- routes/trips.js
+++ routes/trips.js
@@ -6,10 +6,10 @@ const Trip = require('../models/trips');
 
 
 router.get('/', function(req, res) {
-  console.log(moment(req.body.date).format('yyyy-MM-ddTHH:mm:ss.SSS'))
-  console.log(moment(req.body.date).format())
+  // console.log(moment(req.body.date).format('yyyy-MM-ddTHH:mm:ss.SSS'))
+  // console.log(moment(req.body.date).format())
   Trip.find({departure: { $regex: req.body.departure, $options: "i" } , 
-  arrival: { $regex: req.body.arrival, $options: "i" } /*, date: moment(req.body.date).format('yyyy-MM-ddTHH:mm:ss.SSS')*/})
+  arrival: { $regex: req.body.arrival, $options: "i" }, date: { $gte: moment(req.body.date).startOf('day'), $lte: moment(req.body.date).endOf('day') }})
   .then(data => {
     if (data) {
       res.json({result: true, trips: data})
