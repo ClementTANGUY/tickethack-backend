ad131a100f0b89d30c34663cb6bb76945ac15c6c
ClementTANGUY <clemdelatang@gmail.com>
2023-04-12 11:43:31 +0200

    Fix route

--- routes/trips.js
+++ routes/trips.js
@@ -5,10 +5,10 @@ var router = express.Router();
 const Trip = require('../models/trips');
 
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
   // console.log(moment(req.body.date).format('yyyy-MM-ddTHH:mm:ss.SSS'))
   // console.log(moment(req.body.date).format())
-  Trip.find({departure: { $regex: req.body.departure, $options: "i" } , 
+  Trip.find({departure: { $regex: req.body.departure, $options: "i" } ,
   arrival: { $regex: req.body.arrival, $options: "i" }, date: { $gte: moment(req.body.date).startOf('day'), $lte: moment(req.body.date).endOf('day') }})
   .then(data => {
     if (data) {
