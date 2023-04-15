5a06660ebd9212667e8c6dc450346fb44cb1f0ae
ClementTANGUY <clemdelatang@gmail.com>
2023-04-15 09:33:21 +0200

    Add .env file and deport connection string for security

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb+srv://admin:tScVMvZgsSc80ODR@cluster0.ji5pv3n.mongodb.net/Tickethack';
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
  .then(() => console.log('Database connected'))
